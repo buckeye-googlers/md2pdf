57fcbc638ccc9023b6e61655edff863d7e972273
KemingHe <KemingHe@users.noreply.github.com>
2025-08-08 21:05:19 +0000

    deploy: 8d00ce2a19ee9a4abbba95518dee83ea7c2879f4

---
+++ precache-manifest.16408b28ea12811203c8ab880330425e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1581add48510bcbab83a3323ef1945ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ee9f99cacdc0c01d132",
+    "url": "./static/css/2.232cac0f.chunk.css"
+  },
+  {
+    "revision": "b61b8fa0cdc56174e99b",
+    "url": "./static/css/3.fa1beb1f.chunk.css"
+  },
+  {
+    "revision": "66732c276283a53f4206",
+    "url": "./static/css/main.096c7372.chunk.css"
+  },
+  {
+    "revision": "9ee9f99cacdc0c01d132",
+    "url": "./static/js/2.dbb337ff.chunk.js"
+  },
+  {
+    "revision": "610f6619e8cd17771efceae1234b3de0",
+    "url": "./static/js/2.dbb337ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b61b8fa0cdc56174e99b",
+    "url": "./static/js/3.9c0e3c95.chunk.js"
+  },
+  {
+    "revision": "6deb9ada2d2a90964ebad279404033bd",
+    "url": "./static/js/3.9c0e3c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aab124443efa043b627",
+    "url": "./static/js/4.73404abf.chunk.js"
+  },
+  {
+    "revision": "66732c276283a53f4206",
+    "url": "./static/js/main.674104d4.chunk.js"
+  },
+  {
+    "revision": "51ad08054ab304d5371e",
+    "url": "./static/js/runtime-main.1e7f02ed.js"
+  }
+]);
